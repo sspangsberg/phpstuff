a5225fd87a8cc7e28e772e66d62d0bdf46b3f203
Søren Spangsberg Jørgensen <soren@spangsberg.net>
2016-11-29 15:57:18 +0100

    + changed form to send to handleReport.php instead of mailto

--- handleReport.php
+++ handleReport.php
@@ -7,7 +7,7 @@ $email = $_POST['email'];
 
 echo "Thanks for submitting the form.<br/>";
 echo "You were abducted $when_it_happened<br/>";
-echo " and were gone for $how_long";
-echo "Describe them: $alien_description";
-echo "Was Fang there? $fang_spotted";
-echo "You email address is $email";
+echo " and were gone for $how_long<br/>";
+echo "Describe them: $alien_description<br/>";
+echo "Was Fang there? $fang_spotted<br/>";
+echo "You email address is $email<br/>";
